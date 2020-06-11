85f41244377ee602a640487738d799d81414cffa
Mark <mark@mail.com>
2020-06-11 01:51:18 +0100

    extracted containers into a single widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,11 @@ void main() {
 }
 
 class Covid extends StatefulWidget {
-
   @override
   _CovidState createState() => _CovidState();
 }
 
 class _CovidState extends State<Covid> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -37,64 +35,46 @@ class _CovidState extends State<Covid> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('COVID CHECK'),
-        titleSpacing: 2.0,
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        elevation: 5.0,
+        appBar: AppBar(
+          title: Text('COVID CHECK'),
+          titleSpacing: 2.0,
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          elevation: 5.0,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            StatsCard(text: 'Total Confirmed: $confirmed'),
+            StatsCard(text: 'Total Deaths: $deaths'),
+            StatsCard(text: 'Total Recovered: $recovered'),
+          ],
+        ));
+  }
+}
+
+class StatsCard extends StatelessWidget {
+  StatsCard({this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      height: 120.0,
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 25.0,
+          ),
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(15.0),
-            height: 120.0,
-            color: Colors.white,
-            child: Center(
-              child: Text(
-                'Total Confirmed: $confirmed',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25.0,
-                ),
-              ),
-            ),
-            ),
-          Container(
-            margin: EdgeInsets.all(15.0),
-            height: 120.0,
-            color: Colors.white,
-            child: Center(
-              child: Text(
-                'Total Deaths: $deaths',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25.0,
-                ),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(15.0),
-            height: 120.0,
-            color: Colors.white,
-            child: Center(
-              child: Text(
-                'Total Recovered: $recovered',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25.0,
-                ),
-              ),
-            ),
-          ),
-        ],
-      )
     );
   }
 }
