45e4bbf0ea9111eb6b27a36a7cb17bfcb9107103
Mark <mark@mail.com>
2020-06-11 19:02:08 +0100

    added design for cards and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'api_call.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData.dark(),
+    theme: ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: Colors.white,
+    ),
     home: Covid(),
   ));
 }
@@ -16,7 +19,6 @@ class Covid extends StatefulWidget {
 class _CovidState extends State<Covid> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getStats();
   }
@@ -26,10 +28,9 @@ class _CovidState extends State<Covid> {
 
   void getStats() async {
     isWaiting = true;
-
     try {
       var data = await DataCall().getStats();
-      isWaiting = true;
+      isWaiting = false;
       setState(() {
         statsMap = data;
       });
@@ -42,48 +43,65 @@ class _CovidState extends State<Covid> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('COVID CHECK'),
-          titleSpacing: 2.0,
-          centerTitle: true,
-          backgroundColor: Colors.black,
-          elevation: 5.0,
+      appBar: GradientAppBar(
+        title: Text('COVID-19 STATS'),
+        centerTitle: true,
+        elevation: 12.0,
+        gradient: LinearGradient(
+          colors: [Colors.lightBlue, Colors.blue.shade900],
         ),
+      ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StatsCard(text: 'Confirmed', stat: statsMap['confirmed']),
-            StatsCard(text: 'Deaths', stat: statsMap['deaths']),
-            StatsCard(text: 'Recovered', stat: statsMap['recovered']),
+            StatsCard(text: 'Confirmed', stat: isWaiting ? 'Retrieving... ' : statsMap['confirmed'], borderColour: Colors.orangeAccent, shadowColour: Colors.orangeAccent),
+            StatsCard(text: 'Deaths', stat: isWaiting ? 'Retrieving... ' : statsMap['deaths'], borderColour: Colors.red.shade900, shadowColour: Colors.red),
+            StatsCard(text: 'Recovered', stat: isWaiting ? 'Retrieving... ' : statsMap['recovered'], borderColour: Colors.lightGreenAccent, shadowColour: Colors.lightGreenAccent),
           ],
         ));
   }
 }
 
 class StatsCard extends StatelessWidget {
-  StatsCard({this.text, this.stat});
+  StatsCard({this.text, this.stat, this.borderColour, this.shadowColour});
 
   final String text;
   final String stat;
+  final Color borderColour;
+  final Color shadowColour;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Card(
-        margin: EdgeInsets.all(15.0),
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
+      padding: EdgeInsets.all(15.0),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+            colors: [borderColour, Colors.white],
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(15.0)),
         ),
-        elevation: 6.0,
-        child: Text(
-          'Total $text = $stat',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 25.0,
+        child: Card(
+          shadowColor: shadowColour,
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          elevation: 6.0,
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              'Total $text = $stat',
+              style: TextStyle(
+                fontFamily: 'Comfortaa',
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
           ),
         ),
       ),
